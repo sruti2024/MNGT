9592a9deb6f58d2836bb94a649e0237abaf1574e
Sruti Chatterjee <56480052+sruti2024@users.noreply.github.com>
2021-02-05 20:52:34 +0530

    Create frgt_pass.php

---
+++ frgt_pass.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MNGT</title>
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap core CSS -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!--external css-->
+  <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/style-responsive.css" rel="stylesheet">
+  
+</head>
+
+<body>
+  <div id="login-page">
+    <div class="container">
+      <form class="form-login" action="index - Copy.php" method="post">
+        <h2 class="form-login-heading">Verification</h2>
+        <div class="login-wrap">
+        
+          <!-- Code goes here -->
+          
+          <button class="btn btn-theme btn-block"  type="submit"><i class="fa fa-lock"></i> Submit </button>
+          <hr>
+          <div class="registration">
+            Don't have an account yet?<br/>
+            <a class="" href="signup.php">
+              Create an account
+              </a>
+          </div>
+        </div>
+        
+      </form>
+    </div>
+  </div>
+  <!-- js placed at the end of the document so the pages load faster -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <!--BACKSTRETCH-->
+  <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
+  <script type="text/javascript" src="lib/jquery.backstretch.min.js"></script>
+  <script>
+    $.backstretch("img/login-bg.jpg", {
+      speed: 500
+    });
+  </script>
+</body>
+
+</html>
